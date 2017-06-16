5092b9bcf39d52e54b2cbb87c3530dc4c16920fb
ken-mills <ken@pkmills.com>
2017-06-16 13:32:11 -0400

    more cleanup, changed colors

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
-  <carousel :navigationEnabled="false" :pagination="true" :perPage="1" >
-    <slide>
-      <pre class="laravel-pre"><code class="laravel-code">
-class ProjectController extends Controller {
-  public function AddTeamMember(Request $request, Project $project){
-    $new_team_member = new User();
-    $new_team_member->name = $request->input('name');
-    $new_team_member->save();
-    return response()->json([
-      'status' => 'ok',
-    'newTeamMember' => $new_team_member;
-    ]);
-  }
-}
-</code></pre>
-    </slide>
-    <slide>
+<carousel :navigationEnabled="false" :pagination="true" :perPage="1" >
+
+<slide>
+<div id="projects">
+<ul>
+  <li>
+    Your Laravel Project
+  </li>
+  <li>Your Vue.js Project</li>
+</ul>
+</div>
+</slide>
+
+
+<slide>
 <pre class="vue-pre"><code class="vue-code">
 &lt;div v-if="showProjects"&gt;
   &lt;ul&gt;
@@ -29,8 +27,32 @@ class ProjectController extends Controller {
   &lt;/ul&gt;
 &lt;/div&gt;
 </code></pre>
-    </slide>
-  </carousel>
+</slide>
+
+<slide>
+<pre class="laravel-pre"><code class="laravel-code">
+class ProjectController extends Controller {
+  public function AddTeamMember(Request $request, Project $project){
+    $new_team_member = new User();
+    $new_team_member->name = $request->input('name');
+    $new_team_member->save();
+    return response()->json([
+      'status' => 'ok',
+    'newTeamMember' => $new_team_member;
+    ]);
+  }
+}
+</code></pre>
+</slide>
+
+<slide>
+<div id="promo">
+  <h4>One week free, when you add Ken Mills to your team</h4>
+  <h4>Available for remote projects or local projects near Philly</h4>
+</div>
+</slide>
+
+</carousel>
 </template>
 
 <script>
@@ -53,11 +75,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  text-align: left;
 }
 
 .vue-pre{
-  background-color: #35495e;
+  background-color: white;
   min-height: 275px;
+  border:0;
 }
 
 .vue-code {
@@ -69,11 +93,28 @@ export default {
 .laravel-pre{
   background-color: white;
   min-height: 275px;
+  border:0;
 }
 
 .laravel-code {
   color: #f35045;
   font-size: 110%;
+  font-weight: 600;
+}
+
+
+#promo h4{
+  padding-top: 3px;
+  color:#2c3e50;
+  text-align: left;
+}
+
+#projects ul {
+  list-style:none;
+  font-size: 120%;
+}
+#projects li {
+  padding-top:10px;
   font-weight: 600;
 }
 
