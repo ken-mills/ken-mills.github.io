72003c36f6baf34472c865a91b3f8f0fe5d4b813
ken-mills <ken@pkmills.com>
2017-06-16 10:36:29 -0400

    added vue-carousel, redesigning

--- README.md
+++ README.md
@@ -1,21 +1,5 @@
-# ken-mills.github.io
+# pkmills.com
 
-> pkmills.com personal page
+> Personal page for ken-mills on github pages
 
-## Build Setup
-
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
-```
-
-For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+* Integrated slider using vue-carousel in June 2017
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-  </div>
+  <carousel :navigationEnabled="false" :pagination="true" :perPage="1" >
+    <slide>
+      <pre class="laravel-pre"><code class="laravel-code">
+class ProjectController extends Controller {
+  public function AddTeamMember(Request $request, Project $project){
+    $new_team_member = new User();
+    $new_team_member->name = $request->input('name');
+    $new_team_member->save();
+    return response()->json([
+      'status' => 'ok',
+    'newTeamMember' => $new_team_member;
+    ]);
+  }
+}
+</code></pre>
+    </slide>
+    <slide>
+<pre class="vue-pre"><code class="vue-code">
+&lt;div v-if="showProjects"&gt;
+  &lt;ul&gt;
+    &lt;li v-for = "project in projects"&gt;
+      &lt;strong
+        class="fa fa-plus" aria-hidden="true"
+        @click = "addTeamMember( project.id, 'Ken Mills' )"
+      &gt;
+      &lt;/strong&gt;
+    &lt;/li&gt;
+  &lt;/ul&gt;
+&lt;/div&gt;
+</code></pre>
+    </slide>
+  </carousel>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import { Carousel, Slide } from 'vue-carousel';
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Carousel,
+    Slide
   }
 }
 </script>
@@ -25,4 +54,27 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.vue-pre{
+  background-color: #35495e;
+  min-height: 275px;
+}
+
+.vue-code {
+  color: #41b883;
+  font-size: 110%;
+  font-weight: 600;
+}
+
+.laravel-pre{
+  background-color: white;
+  min-height: 275px;
+}
+
+.laravel-code {
+  color: #f35045;
+  font-size: 110%;
+  font-weight: 600;
+}
+
 </style>
