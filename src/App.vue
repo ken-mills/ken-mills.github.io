597662fb1250a29192823891a49790b136fc0f2d
ken-mills <ken@pkmills.com>
2017-06-16 20:58:21 -0400

    promo changes

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
 
 <div>
 
-<div id="projects" v-if="showForm">
+<div id="projects" v-if="showForm1">
 
       <table>
       <tr v-for="project in projects">
           <td>
               <div class="truncate">
                 <span>Add a Ken Mills to your </span>
-                <span :class="{'laravel-code': project.isLaravel, 'vue-code': !project.isLaravel}">
+                <span :class="project.class">
                   {{project.name}}
                 </span>
                 <span class="truncated"> project </span>
@@ -26,50 +26,73 @@
       </table>
 </div>
 
-  <carousel v-if="showCarousel" :navigationEnabled="false" pagination="true" perPage="1" paginationColor="#D4D3D3" >
+<div id="ken-mills" v-if="showForm2">
 
+  <form method="POST" v-on:submit.prevent="saveTeamMember" class="form-inline" >
 
-  <slide>
-  <pre class="vue-pre"><code class="vue-code">
-  &lt;div v-if="showProjects"&gt;
-    &lt;ul&gt;
-      &lt;li v-for = "project in projects"&gt;
-        &lt;strong
-          class="fa fa-plus" aria-hidden="true"
-          @click = "addTeamMember( project.id, 'Ken Mills' )"
-        &gt;
-        &lt;/strong&gt;
-      &lt;/li&gt;
-    &lt;/ul&gt;
-  &lt;/div&gt;
-  </code></pre>
-  </slide>
-
-  <slide>
-  <pre class="laravel-pre"><code class="laravel-code">
-  class ProjectController extends Controller {
-    public function AddTeamMember(Request $request, Project $project){
-      $new_team_member = new User();
-      $new_team_member->name = $request->input('name');
-      $new_team_member->save();
-      $project->add_member($new_team_member);
-      return response()->json([
-        'status' => 'ok',
-        'newTeamMember' => $new_team_member;
-      ]);
-    }
-  }
-  </code></pre>
-  </slide>
-
-  <slide>
-  <div id="promo">
-    <h4><span style="text-decoration: underline">One week free</span>, when you add Ken Mills to your team</h4>
-    <h4>Available for remote projects or onsite projects near Philly</h4>
+  <div class="form-group form-spacing">
+    <label for="member">Team Member</label>
+    <input type="text" class="form-control" value="Ken Mills" id="member" disabled>
   </div>
-  </slide>
 
-  </carousel>
+  <div class="form-group">
+    <input type="submit" value="Add" class="btn btn-primary" />
+  </div>
+
+  </form>
+
+</div>
+
+<carousel v-if="showCarousel" :navigationEnabled="false" pagination="true" perPage="1" paginationColor="#D4D3D3" >
+
+
+<slide>
+<h4>You get a Vue.js developer</h4>
+<pre class="vue-pre"><code class="vue-code">
+&lt;div v-if="showProjects"&gt;
+  &lt;ul&gt;
+    &lt;li v-for = "project in projects"&gt;
+      &lt;strong
+        class="fa fa-plus" aria-hidden="true"
+        @click = "addTeamMember( project.id, 'Ken Mills' )"
+      &gt;
+      &lt;/strong&gt;
+    &lt;/li&gt;
+  &lt;/ul&gt;
+&lt;/div&gt;
+</code></pre>
+</slide>
+
+<slide>
+<h4>You get a Laravel developer</h4>
+<pre class="laravel-pre"><code class="laravel-code">
+class ProjectController extends Controller {
+  public function AddTeamMember(Request $request, Project $project){
+    $new_team_member = new User();
+    $new_team_member->name = $request->input('name');
+    $new_team_member->save();
+    $project->add_member($new_team_member);
+    return response()->json([
+      'status' => 'ok',
+      'newTeamMember' => $new_team_member;
+    ]);
+  }
+}
+</code></pre>
+</slide>
+
+<slide>
+<div id="promo">
+ <ul>
+  <li>You get a team player</li>
+  <li>You get an experienced IS professional</li>
+  <li>You get <span style="text-decoration: underline">one week free</span>, when you add Ken Mills to your team</li>
+  <li>You get a remote developer or an onsite dev near Philly</li>
+  </ul>
+</div>
+</slide>
+
+</carousel>
 
 </div>
 </template>
@@ -85,19 +108,20 @@ export default {
   },
   data: function () {
     return {
-      showForm: true,
+      showForm1: true,
+      showForm2: false,
       showCarousel:false,
       projects: [
 
-        {"id":0, "name" : "Laravel" , "isLaravel" : true },
-        {"id":1, "name" : "Vue.js", "isLaravel" : false },
+        {"id":0, "name" : "Laravel" , "class" : "laravel-code" },
+        {"id":1, "name" : "Vue.js", "class" : "vue-code" },
 
       ]
     }
   },
 
   mounted: function () {
-    this.showForm = true;
+    this.showForm1 = true;
   },
 
   methods: {
@@ -105,8 +129,15 @@ export default {
     addTeamMember: function(id){
 
       console.log('Button clicked = ' + id);
-      this.showForm = false;
+      this.showForm1 = false;
       this.showCarousel = true;
+    },
+
+    saveTeamMember: function(id){
+
+      console.log('Button clicked = ' + id);
+      this.showForm2 = false;
+      this.showCarousel = false;
     }
 
   }
@@ -119,7 +150,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
   text-align: left;
 }
 
@@ -163,9 +194,10 @@ export default {
   font-weight: 600;
 }
 
-#promo h4{
+#promo li{
   padding-top: 3px;
   color:#2c3e50;
+  font-size: 150%;
   text-align: left;
 }
 
@@ -176,4 +208,10 @@ export default {
 a:hover {
  cursor:pointer;
 }
+
+.form-spacing{
+  padding-left: 15px;
+}
+
+
 </style>
